262b2c01595a74197b1b7c07b9723fa405af5425
zxcdrtmylo <rugratsmylo@gmail.com>
2014-10-08 00:10:13 -0400

    tengo un problema con la base de datos ing :( no me corre

--- triviadormylo/apps/usuarios/forms.py
+++ triviadormylo/apps/usuarios/forms.py
@@ -2,13 +2,11 @@ from django import forms
 from django.forms import ModelForm
 from .models import *
 import pdb
-
-#class Usuarios(ModelForm):
-#	class meta:
+tipos=(('private','Privado'),('public','Publico'),('protected','Protegido'),)
 class comentarios(ModelForm):
 	class Meta:
 		model=comentarios
 		exclude=["Usuarios"]
-class categorias(ModelForm):
+class preguntas(ModelForm):
 	class Meta:
-		model=categorias
+		model=preguntas
--- triviadormylo/apps/usuarios/urls.py
+++ triviadormylo/apps/usuarios/urls.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .views import *
 
 urlpatterns = patterns('',
-	url(r'^$', pagina_principal)
+	url(r'^$', pagina_principal),
+	url(r'^comentarios/$', pagina_comentarios),
+	url(r'^registro/$', registro_usuarios),
+	url(r'^login/$', login_usuario),
+	url(r'^logout/$', logout_usuario),
+	url(r'^perfil/$', perfil),
+	url(r'^preguntas/$', preguntas),
 )
